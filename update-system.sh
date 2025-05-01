a035cf986a19a056d7df15e9394deefe43060207
Gabriel Palassi <gabrielpalassi@hotmail.com>
2025-05-01 17:41:49 -0300

    Run rclone self-update as root

--- update-system.sh
+++ update-system.sh
@@ -4,9 +4,9 @@
 sudo dnf upgrade --refresh -y
 sudo dnf autoremove -y
 
+# Update rclone
+sudo rclone self-update
+
 # Update Flatpak packages and clean up
 flatpak update -y
 flatpak uninstall --unused -y
-
-# Update rclone
-rclone selfupdate
