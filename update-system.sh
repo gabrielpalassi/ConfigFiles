eb2407f221c395fecfa8bb9580b222139953224c
Gabriel Palassi <gabrielpalassi@hotmail.com>
2025-10-19 03:12:52 -0300

    Include firmware updates in update script and update vscode settings

--- update-system.sh
+++ update-system.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Update system packages and clean up
-echo "[Info] Updating system packages..."
-sudo dnf upgrade --refresh -y
-sudo dnf autoremove -y
+# Authenticate sudo session
+sudo -v
 
 # Update Flatpak packages and clean up
 echo "[Info] Updating Flatpak packages..."
 flatpak update -y
 flatpak uninstall --unused -y
 
+# Update system packages and clean up
+sudo -v
+echo "[Info] Updating system packages..."
+sudo dnf upgrade --refresh -y
+sudo dnf autoremove -y
+
 # Update individual applications
+sudo -v
 echo "[Info] Updating rclone..."
 sudo rclone selfupdate
 echo "[Info] Updating npm..."
@@ -21,3 +26,9 @@ echo "[Info] Updating bun..."
 bun upgrade
 echo "[Info] Updating pip..."
 pip install --upgrade pip
+
+# Update firmware
+sudo -v
+echo "[Info] Updating firmware..."
+sudo fwupdmgr refresh --force
+sudo fwupdmgr update -y
